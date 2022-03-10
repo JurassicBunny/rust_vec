8018a96060a51f007be57255696e6e3ce9d6bf77
Matthew <matthew@Matthews-MBP.attlocal.net>
2022-03-10 07:02:02 -0500

    correct benchmarks

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,14 +1,17 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use vectored::Vector3D;
+use vectored::{Acceleration, Vectored};
 
 fn benches(c: &mut Criterion) {
-    let vector = Vector3D::new(1.0, 2.0, 3.0);
-    c.bench_function("Vector3D::new()", |b| {
-        b.iter(|| Vector3D::new(1.0, 2.0, 3.0))
+    let vector = Acceleration::new(1.0, 2.0, 3.0);
+    let vector_2 = Acceleration::new(1.0, 2.0, 3.0);
+    c.bench_function("Vectored::new()", |b| {
+        b.iter(|| Acceleration::new(1.0, 2.0, 3.0))
     });
-    c.bench_function("Vector3D::norm()", |b| b.iter(|| vector.norm()));
-    c.bench_function("Vector3D::sqr_norm()", |b| b.iter(|| vector.sqr_norm()));
-    c.bench_function("Vector3D::normalize()", |b| b.iter(|| vector.normalize()));
+    c.bench_function("Vectored::norm()", |b| b.iter(|| vector.norm()));
+    c.bench_function("Vectored::sqr_norm()", |b| b.iter(|| vector.sqr_norm()));
+    c.bench_function("Vectored::normalize()", |b| b.iter(|| vector.normalize()));
+    c.bench_function("Vectored::addition()", |b| b.iter(|| vector + vector_2));
+    c.bench_function("Vectored::subtraction()", |b| b.iter(|| vector - vector_2));
 }
 
 criterion_group!(my_bench, benches);
